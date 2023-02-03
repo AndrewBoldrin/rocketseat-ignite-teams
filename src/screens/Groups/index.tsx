e211c717da533ba2f832afca1be15beefc7f81fa
andrew boldrin <andrewboldrin@hotmail.com>
2023-02-02 22:21:58 -0300

    feat: adicionando empty list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import { Header } from "components/Header";
 import { Highlight } from "components/Highlight";
 import { GroupCard } from "components/GroupCard";
 import { Container } from "./styles";
+import { FlatList } from "react-native";
+import { ListEmpty } from "components/ListEmpty";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="galera do ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   );
 }
