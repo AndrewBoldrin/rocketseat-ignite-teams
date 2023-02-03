715c630186b5b71f255f4d883f00394a85f2ca36
andrew boldrin <andrewboldrin@hotmail.com>
2023-02-03 12:40:48 -0300

    refactor: adicionando css helper nos styled components

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 } from "@expo-google-fonts/roboto";
 import theme from "./src/theme";
 import { Loading } from "components/Loading";
-import { NewGroup } from "screens/NewGroup";
+import { Groups } from "screens/Groups";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <NewGroup /> : <Loading />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
