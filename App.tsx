31e6788a770f71fdc548084319eca0dd89759c28
andrew boldrin <andrewboldrin@hotmail.com>
2023-02-03 18:37:34 -0300

    feat: adicionando componente ButtonIcon

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 } from "@expo-google-fonts/roboto";
 import theme from "./src/theme";
 import { Loading } from "components/Loading";
-import { Groups } from "screens/Groups";
+import { Players } from "screens/Players";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
