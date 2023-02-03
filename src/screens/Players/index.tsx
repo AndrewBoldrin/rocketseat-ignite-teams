e36e9b4d12f497b629e0bd7b835639deb3037833
andrew boldrin <andrewboldrin@hotmail.com>
2023-02-03 19:49:04 -0300

    feat: adicionando player card list

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,6 +6,9 @@ import { ButtonIcon } from "components/ButtonIcon";
 import { Input } from "components/Input";
 import { Filter } from "components/Filter";
 import { FlatList } from "react-native";
+import { PlayerCard } from "components/PlayerCard";
+import { ListEmpty } from "components/ListEmpty";
+import { Button } from "components/Button";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
@@ -38,6 +41,23 @@ export function Players() {
         />
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time." />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+      />
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }
