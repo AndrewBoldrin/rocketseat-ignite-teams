02e517087e968a985d55a5cde427a80a17b46baa
andrew boldrin <andrewboldrin@hotmail.com>
2023-02-05 15:45:57 -0300

    feat: adicionando navegação entre screens

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { BackIcon, Container, Logo, BackButton } from "./styles";
+import { useNavigation } from "@react-navigation/native";
 import logoImg from "assets/logo.png";
 
 type Props = {
@@ -6,10 +7,16 @@ type Props = {
 };
 
 export function Header({ showBackButton = false }: Props) {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.navigate("groups");
+  }
+
   return (
     <Container>
       {showBackButton && (
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       )}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,11 +4,18 @@ import { Highlight } from "components/Highlight";
 import { GroupCard } from "components/GroupCard";
 import { Container } from "./styles";
 import { FlatList } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { ListEmpty } from "components/ListEmpty";
 import { Button } from "components/Button";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate("new");
+  }
 
   return (
     <Container>
@@ -23,7 +30,7 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   );
 }
