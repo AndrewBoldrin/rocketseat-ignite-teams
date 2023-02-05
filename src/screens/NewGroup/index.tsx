a1f6a2d526037f4fc1d138a8d6c770313cb959d9
andrew boldrin <andrewboldrin@hotmail.com>
2023-02-05 16:52:51 -0300

    feat: carregamento da lista de groups

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -5,13 +5,19 @@ import { Highlight } from "components/Highlight";
 import { Input } from "components/Input";
 import { Container, Content, Icon } from "./styles";
 import { useNavigation } from "@react-navigation/native";
+import { groupCreate } from "storage/group/groupCreate";
 
 export function NewGroup() {
   const [group, setGroup] = useState("");
   const navigation = useNavigation();
 
-  function handleNew() {
-    navigation.navigate("players", { group });
+  async function handleNew() {
+    try {
+      await groupCreate(group);
+      navigation.navigate("players", { group });
+    } catch (error) {
+      throw error;
+    }
   }
 
   return (
